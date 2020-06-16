13d2037fed0c2a8b9a7c3e1586982db51fadae17
Tom Kota <63372913+tom2kota@users.noreply.github.com>
2020-06-16 15:48:47 +0300

    Add styles

--- App.js
+++ App.js
@@ -1,19 +1,28 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>Hi There!</Text>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#36b4a1',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    text: {
+        fontStyle: 'italic',
+        fontSize: 60,
+        fontWeight: 'bold',
+        textDecorationColor: '#ffcd06',
+        textShadowColor: '#ffffff',
+        color: '#ffcd06'
+    }
+
 });
